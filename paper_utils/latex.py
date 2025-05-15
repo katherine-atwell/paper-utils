05c59fe41bf9e30b94b9de46c5ee6a096fbe3167
katherine-atwell <34731305+katherine-atwell@users.noreply.github.com>
2025-05-15 14:26:13 -0400

    update data type

--- paper_utils/latex.py
+++ paper_utils/latex.py
@@ -1,4 +1,14 @@
-def to_latex(df, caption=None, label=None, index=False):
+from utils import *
+
+def to_latex(data, caption=None, label=None, index=False):
+    """
+    Convert a DataFrame to a LaTeX table with custom formatting.
+    Args:
+        data (List[dict]): List of dictionaries containing the data to be converted.
+        caption (str): The caption for the LaTeX table.
+        label (str): The label for the LaTeX table.
+        index (bool): Whether to include the index in the LaTeX table."""
+    df = json_to_df(data)
     latex_table_custom = df.to_latex(
             index=index,
             header=True,
